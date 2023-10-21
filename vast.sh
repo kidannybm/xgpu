bc42e61c70f5f3ad0750beacf9e88b20ddffc998
kidannybm <137231924+kidannybm@users.noreply.github.com>
2023-10-20 23:37:46 -0400

    Update vast.sh

--- vast.sh
+++ vast.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x1d166521aB1A33a5CE18A1c6E32F3B07096E142a/g' config.conf
 sleep 5
 echo "---------------------------"
 
